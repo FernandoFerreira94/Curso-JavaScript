f35a34683d0a906b3ffc7871e932aaa4b89d3fc3
Fernando Ferreira <fernandoeqp59@gmail.com>
2024-09-11 21:30:52 -0300

    atividade 4 finalizada

--- exercicio/atividade1.js
+++ exercicio/atividade1.js
@@ -8,17 +8,17 @@ Se a idade informada for maior ou igual a 60, exibir a mensagem “Idoso”.
 */
 
 
-
+// -------------- Idade -------------
 function calIdade() {
     let idade = document.getElementById('idade').value
     document.getElementById('idade-final').style.background = 'white'
-    if (idade > 0 && idade <= 15) {
+    if (idade >= 0 && idade <= 11) {
         document.getElementById('idade-final').innerText = 'Criança'
         document.getElementById('idade').value = ''
-    } else if (idade > 15 && idade <= 30) {
-        document.getElementById('idade-final').innerText = 'Jovem'
+    } else if (idade >= 11 && idade <= 17) {
+        document.getElementById('idade-final').innerText = 'Adolecente'
         document.getElementById('idade').value = ''
-    } else if (idade > 30 && idade <= 59) {
+    } else if (idade >= 18 && idade <= 59) {
         document.getElementById('idade-final').innerText = 'Adulto'
         document.getElementById('idade').value = ''
     } else {
@@ -32,21 +32,105 @@ function calIdade() {
 
 
 function calImc() {
-    let nome = document.getElementById('info-nome').value
-    let alturaCem = document.getElementById('info-altura').value
-    let altura2 = alturaCem / 100
-    let peso1 = document.getElementById('info-peso').value
-    let imc = peso1 / (altura2 * altura2)
+    let nome = document.getElementById('info-nome')
+    let alturaCem = document.getElementById('info-altura')
+    let altura = alturaCem.value / 100
+    let peso = document.getElementById('info-peso')
+    let imc = peso.value / (altura * altura)
 
-    document.getElementById('nome').innerText = `Olá ${nome}`;
-    document.getElementById('altura').innerText = `Altura: ${altura2}`;
-    document.getElementById('peso').innerText = `Peso: ${peso1}`
+    document.getElementById('nome').innerText = `Olá ${nome.value}`;
+    document.getElementById('altura').innerText = `Altura: ${altura}`;
+    document.getElementById('peso').innerText = `Peso: ${peso.value}`
     document.getElementById('imc').innerText = `IMC: ${imc}`
 
-    document.getElementById('info-nome').value = ''
-    document.getElementById('info-altura').value = ''
-    document.getElementById('info-peso').value = ''
-
+    nome.value = ''
+    alturaCem.value = ''
+    peso.value = ''
 }
+
+//----------- calculo soma e subtração ----------
+
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+let calResultado = document.getElementById('calResultado')
+
+let cal = function (numero1, numero2, operação) {
+
+    // calculadora If Else
+
+    /*    if (operação == '+') {
+            resultado = numero1 + numero2
+            calResultado.innerHTML = resultado
+        } else if (operação == '-') {
+            resultado = numero1 - numero2
+    
+            calResultado.innerHTML = resultado
+        } else if (operação == '*') {
+            resultado = numero1 * numero2
+            calResultado.innerHTML = resultado
+        } else if (operação == '/') {
+            resultado = numero1 / numero2
+            calResultado.innerHTML = resultado
+        }
+        num1.value = ''
+        num2.value = ''
+        */
+
+    // calculadora switch
+
+    switch (operação) {
+        case '+':
+            resultado = numero1 + numero2
+            break;
+        case '-':
+            resultado = numero1 - numero2
+            break;
+        case '*':
+            resultado = numero1 * numero2
+            break;
+        case '/':
+            resultado = numero1 / numero2
+            break;
+        default:
+            resultado = ('operação Invalida')
+            break;
+
+    }
+    calResultado.innerHTML = resultado
+    num1.value = ''
+    num2.value = ''
+}
+
+// ------------- mudando cor de fundo texto ---------
+var letra = document.getElementById('atv4').innerHTML
+
+function bg(ativo)  {
+   switch (ativo) {
+       case 'foco':
+           document.getElementById('atv4').className = 'foco'
+           letra = ''
+           
+           break;
+           case 'blur':
+               document.getElementById('atv4').className = 'blur'
+               letra = ''
+        break;
+    default:
+        break;
+   }
+console.log(letra.length)
+}
+
+function escrever() {
+    letra += letra.length   
+    
+    console.log(letra.length)
+    if (letra.length <= 5) {
+        document.getElementById('atv4').className = 'menor5'
+     } else if (letra.length > 5  ) {
+            document.getElementById('atv4').className = 'maior5'            
+     }
+}
+
 
 
