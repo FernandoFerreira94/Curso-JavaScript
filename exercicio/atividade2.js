8f3f7956467e6788964337b82e57861d2dad2a4e
Fernando Ferreira <fernandoeqp59@gmail.com>
2024-09-16 17:05:54 -0300

    atividade finalizada

--- exercicio/atividade2.js
+++ exercicio/atividade2.js
@@ -2,14 +2,37 @@
 
 //-------- Lista de feira ---------
 let adc = document.getElementById('feira')
-var lista = document.getElementById('lista1')
+var campoLista = document.getElementById('lista1')
+var count = 0
+var element = []
 
-function adcicionar(acao) {
+function adicionar(acao) {
     if (acao=='feira') {
-        lista.innerHTML += `<li>${adc.value}</li>` 
-        adc.value =''
-        
-    } else if (acao == 'clear') {
-        lista.innerHTML = ''
-    }
+        if (!adc.value == ''  ) { 
+            if ( element.indexOf(adc.value) == -1) {
+                element.push(adc.value)
+                campoLista.innerHTML += `<li >${element[count]}</li>`
+                count ++
+            } else {alert('Esse item ja foi adicionado')}
+            adc.value = ''
+        } else {
+            alert('adicione um item')
+            adc.value = ''
+        }
+    }  
 }
+
+function ordenar() {
+    element.sort()
+        campoLista.innerHTML = ''
+        for (let i = 0; count > i; i++) {
+            campoLista.innerHTML += `<li>${element[i]}</li>`
+        }
+}
+
+function limpar() {
+      return  campoLista.innerHTML = ''
+}
+
+
+
